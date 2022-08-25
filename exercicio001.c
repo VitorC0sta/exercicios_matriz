0e79fc2821317fca915c0123595fcceba1e0880c
Vitor Costa <vitor.costafelippe@gmail.com>
2022-08-25 20:43:15 -0300

    primeiro programa

--- exercicio001.c
+++ exercicio001.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void matriz();
+
 int main()
 {
 
 
+ matriz();
  
+ getchar();
  return 0;
+
+}
+
+void matriz()
+{
+ int i,j, cont = 0;
+
+ int matriz[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+ for(i = 0; i < 4; i++){
+    printf("\t\t");
+    for(j = 0; j < 4; j++){
+        printf("%d\t", matriz[i][j]);
+        if(matriz[i][j] > 10){
+            cont++;
+        }
+    }
+        printf("\n");
+ }
+ printf("\n\tValores > 10 na matriz: %d", cont);  
 
 }
