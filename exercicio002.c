6ff3a86b790fd1cb2326f2c06d42f7d11a65f9f0
Vitor Costa <vitor.costafelippe@gmail.com>
2022-08-27 10:21:06 -0300

    Correção do exercicio 002

--- exercicio002.c
+++ exercicio002.c
@@ -3,13 +3,21 @@
 
 int main() 
 {
-    int matriz[5][5] = {0,0,0,0,0};
+    int matriz[5][5];
     int i,j;
 
-    for(i = 1; i < 5; i++) {
-            printf("\tDigite 5 valores para linha %d: ", i);
+    for(i = 0; i < 5; i++) {
+            
         for(j = 0; j < 5; j++){
-            scanf("%d%*c", &matriz[i][j]);
+            if(i == j){
+            
+            matriz[i][j] = 1;
+
+            } else {
+
+                matriz[i][j] = 0;
+
+            }
         }
     }
 
